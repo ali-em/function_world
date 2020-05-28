68f9f9b0a2b4b86a87f96d8105643b3f157ad145
alithecodeguy <alithecodeguy@gmail.com>
2020-05-29 00:20:33 +0430

    Create useWindowSize.js

---
+++ functions/JavaScript/useWindowSize.js
@@ -0,0 +1,24 @@
+// window size custome hook
+export const useWindowSize = () => {
+  const isClient = typeof window === 'object';
+  const getSize = () => {
+    return {
+      width: isClient ? window.innerWidth : undefined, 
+      height: isClient ? window.innerHeight : undefined,
+    };
+  };
+  const [windowSize, setWindowSize] = useState(getSize);
+  useEffect(() => {
+    if (!isClient) {
+      return false;
+    }
+    function handleResize() {
+      setWindowSize(getSize());
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []); // Empty array ensures that effect is only run on mount and unmount
+
+  return windowSize;
+};
